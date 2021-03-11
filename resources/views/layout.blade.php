91866d329611bfdf73e6bf6d1b386702692bb13a
marlieer <marlie_97@hotmail.ca>
2021-03-11 10:01:37 -0800

    update layout

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,4 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>@yield('title', 'Team Planner')</title>
 
-<div class="container">
-    @yield('content')
-</div>
+    <!--Style-->
+@yield('header')
+
+<!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="{{ URL::asset('css/style.css') }}" rel="stylesheet">
+
+
+</head>
+<body>
+<header>
+    <!-- Top nav bar -->
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+        <a href="{{ route('project.index') }}" class="navbar-brand">Team Planner</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse"
+                aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a href="{{ route('project.index') }}"
+                       class="nav-link {{$_SERVER['REQUEST_URI'] == '/project' ? ' active' : ''}}">Projects</a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{ route('team_member.index') }}"
+                       class="nav-link {{$_SERVER['REQUEST_URI'] == '/team_member' ? ' active' : ''}}">Signifly Team</a>
+                </li>
+            </ul>
+
+            <!--Right side of nav bar-->
+            <ul class="navbar-nav justify-content-end">
+            </ul>
+        </div>
+    </nav>
+</header>
+<main>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+
+    <div class="container">
+        @if ($errors->any())
+            @foreach($errors->all() as $error)
+                <div class="alert alert-danger">
+                    <p>
+                        {{ $error }}
+                    </p>
+                </div>
+            @endforeach
+        @endif
+
+        @if (Session::has('success'))
+            <div class="alert alert-success">
+                <ul>
+                    <li>{{ Session::get('success') }}</li>
+                </ul>
+            </div>
+        @endif
+        <br>
+        <div class="container">
+            @yield('content')
+        </div>
+        <div class="container create-project">
+            <a href="{{route('project.create')}}" class="button button-success">Create New Project</a>
+        </div>
+    </div>
+</main>
+</body>
+</html>
+
