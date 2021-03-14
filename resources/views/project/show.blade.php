151cacbb0b1b7174e5b2e573bdd56c6b417b6422
marlieer <marlie_97@hotmail.ca>
2021-03-14 16:18:04 -0700

    show project with team members

--- resources/views/project/show.blade.php
+++ resources/views/project/show.blade.php
@@ -1,7 +1,29 @@
 @extends('layout')
 
 @section('content')
-    <h1>{{ $project->title }}</h1>
-    <p>{{ $project->client }}</p>
-    <p>{{ $project->description }}</p>
+    <div class="container">
+        <h1>{{ $project->title }}</h1>
+        <h3>Client: {{ $project->client }}</h3>
+        <p>{{ $project->description }}</p>
+    </div>
+
+    <div class="container">
+        <h3>Project Team Members</h3>
+        <div class="row">
+            @foreach($project->teamMembers as $team_member)
+                <div class="col-md col-sm-12 d-flex align-items-stretch">
+                    <div class="card" style="width: 18rem;">
+                        <img class="card-img-top" src="{{ asset($team_member->profile_image)}}" alt="people">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $team_member->name }}</h5>
+                            <p class="card-subtitle">{{ $team_member->job_title }}</p>
+                            <p class="card-text">{{ $team_member->email }}</p>
+                            <a target="_blank" href="{{ route('team_member.show', ['team_member' => $team_member->id]) }}">Learn More</a>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+
 @endsection
